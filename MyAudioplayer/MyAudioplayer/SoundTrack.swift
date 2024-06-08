259feb7d8e92af5aa48835a9832b550fbfca91d8
Dinur Balov <119072459+dinur0@users.noreply.github.com>
2024-06-08 12:36:45 +0300

    Update SoundTrack.swift

--- MyAudioplayer/MyAudioplayer/SoundTrack.swift
+++ MyAudioplayer/MyAudioplayer/SoundTrack.swift
@@ -1,58 +1,44 @@
-//
-//  SoundTrack.swift
-//  MyAudioplayer
-//
-//  Created by хех on 07.06.2024.
-//
-
 import UIKit
 import AVFoundation
 
 class SoundTrack: UIViewController {
 
-    var player: AVAudioPlayer?
-    var isPlaying = true
+    var player: AVPlayer?
+    var isPlaying = false
+    var trackURL: URL?
+    
+    @IBOutlet weak var songTitleLabel: UILabel!
+    @IBOutlet weak var songArtistLabel: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationTimeLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bundle
-        player = AVPlayer (url: URL(fileURLWithPath:Bundle.main
+        if let url = trackURL {
+            player = AVPlayer(url: url)
+            songTitleLabel.text = url.lastPathComponent
+        }
     }
     
-    @IBOutlet weak var songTitleLabel: UILabel!
-
-    @IBOutlet weak var songArtistLabel: UILabel!
-    
-    @IBOutlet weak var currentTimeLabel: UILabel!
-    
-    @IBOutlet weak var durationTimeLabel: UILabel!
-    
-    @IBOutlet weak var playButton: UIButton!
-    
     @IBAction func previousMusicButton(_ sender: UIButton) {
+        // Implement previous track functionality
     }
     
     @IBAction func nextMusicButton(_ sender: UIButton) {
+        // Implement next track functionality
     }
     
     @IBAction func playOrPause(_ sender: UIButton) {
-        if(!isPlaying){
-            player?.play()
-            isPlaying = true
-        } else {
-            player?.pause()
-            isPlaying = false
+        if let player = player {
+            if isPlaying {
+                player.pause()
+                playButton.setTitle("Play", for: .normal)
+            } else {
+                player.play()
+                playButton.setTitle("Pause", for: .normal)
+            }
+            isPlaying.toggle()
         }
     }
-    /*
-     
-     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
